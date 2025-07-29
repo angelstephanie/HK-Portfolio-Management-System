e98d1a9e420735ff9229609c1a04920e459fc19e
Lareb Zulfqar <larebzulfqar@gmail.com>
2025-07-29 08:57:02 +0000

    Updated transaction service

--- backend/service/Transaction_service.py
+++ backend/service/Transaction_service.py
@@ -7,8 +7,11 @@ class TransactionService:
     def add_transaction(self, transaction):
         self.transaction_repo.add_transaction(transaction)
 
-    def get_transactions_by_portfolio_id(self, portfolio_id: int):
-        return self.transaction_repo.get_transactions_by_portfolio_id(portfolio_id)
+    def get_transaction_by_id(self, transaction_id: int):
+        return self.transaction_repo.get_transaction_by_id(transaction_id)
+    
+    def get_all_transactions(self):
+        return self.transaction_repo.get_all_transactions()
 
     def delete_transaction(self, transaction_id: int):
         self.transaction_repo.delete_transaction(transaction_id)
