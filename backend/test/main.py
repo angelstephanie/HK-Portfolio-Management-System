21d3c096d924a55f54e3d9ce13ebca07c9f249b1
fayehe <fayehe777@gmail.com>
2025-08-06 03:53:07 +0000

    fix test main.py

--- backend/test/main.py
+++ backend/test/main.py
@@ -38,7 +38,7 @@ def run_tests():
     test_PortfolioSnap_repo.run_tests()
     test_Transaction_repo.run_tests()
     
-    # # Run controllers tests   
+    # Run controllers tests   
     test_loader = unittest.TestLoader()
     test_suite = test_loader.discover('backend/test/controllers', pattern='test_*.py')
     test_runner = unittest.TextTestRunner()
