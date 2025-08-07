fd1f15946d566380723eb21729c16378603f247d
fayehe <fayehe777@gmail.com>
2025-08-07 02:20:54 +0000

    add watchlist repo

---
+++ backend/repository/Watchlist_repo.py
@@ -0,0 +1,51 @@
+from backend.models.Watchlist import Watchlist
+from backend.models.Asset import Asset, AssetType
+from backend.repository.database_access import get_database_connection
+
+class Watchlist_repo:
+    def __init__(self):
+        self.connection = get_database_connection()
+    
+    def add_asset2watchlist(self, symbol :str):
+        """Add a new asset symbol to the watchlist."""
+        try:
+            cursor = self.connection.cursor()
+            cursor.execute("""
+                            INSERT INTO Watchlist (symbol)
+                            VALUES (%s)
+                            """, (symbol,))
+
+            self.connection.commit()
+            affected_rows = cursor.rowcount
+            cursor.close()
+            print(f"✅ Watchlist updated: {symbol}")
+            
+            return affected_rows if affected_rows > 0 else None
+        
+        except Exception as e:
+            print(f"❌ Error updating Watchlist: {e}")
+            return None
+    
+    def get_watchlist(self):
+        """Retrieve all assets in the watchlist."""
+        try:
+            cursor = self.connection.cursor()
+            cursor.execute("SELECT * FROM Watchlist LEFT JOIN Assets USING symbol")
+            rows = cursor.fetchall()
+            cursor.close()
+            
+            assets = []
+            for row in rows:
+                assets.append(Asset(
+                    symbol=row[0],
+                    name=row[1],
+                    type=AssetType(row[2]),
+                    current_price=row[3],
+                    opening_price=row[4],
+                    last_updated=row[5]
+                ))
+            print(f"✅ Watchlist retrieved {len(assets)} assets")
+            return assets
+        except Exception as e:
+            print(f"❌ Error retrieving watchlist: {e}")
+            return None
--- setup.py
+++ setup.py
@@ -1,16 +1,19 @@
 from backend.YahooFetcher import YahooFetcher
 from backend.repository.Asset_repo import Asset_repo
+from backend.repository.Watchlist_repo import Watchlist_repo
 
 def setup():
     # Create an instance of AssetService
     yahooFetcher = YahooFetcher()
     asset_repo = Asset_repo()
+    watchlist_repo = Watchlist_repo()
     
     # Fetch and update assets
     assets = yahooFetcher.fetchByAssetType()
     for asset in assets:
         print(f"Adding asset: {asset}")
         asset_repo.add_asset(asset)
+        watchlist_repo.add_asset2watchlist(asset.symbol)
     
 if __name__ == "__main__":
     setup()
